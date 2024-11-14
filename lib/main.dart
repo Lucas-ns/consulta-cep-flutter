cfd4fbed623a9c15654722bf6919eaf329efc520
Lucas Silva <nascimentolucas786@gmail.com>
2024-11-14 16:19:37 -0300

    Config do projeto

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,8 @@
-import 'package:consulta_cep/pages/main_screen.dart';
+import 'package:consulta_cep/my_app.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-void main() {
+void main() async {
+  await dotenv.load(fileName: ".env");
   runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MainScreen(title: 'Consultar CEP'),
-      debugShowCheckedModeBanner: false,
-    );
-  }
 }
